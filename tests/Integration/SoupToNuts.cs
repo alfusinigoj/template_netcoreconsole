5059aea867ad7025c98d85b0282e2e1c88d05311
Alfus Inigo Jaganathan <ajaganathan@pivotals-mbp.lan>
2018-11-01 10:31:16 -0400

    initial

--- tests/Integration/SoupToNuts.cs
+++ tests/Integration/SoupToNuts.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 
-namespace tests
+namespace <%= namespace %>.Integration
 {
     public class SoupToNuts
     {
--- tests/Unit/UnitTest1.cs
+++ tests/Unit/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 
-namespace tests
+namespace <%= namespace %>.Unit
 {
     public class UnitTest1
     {
